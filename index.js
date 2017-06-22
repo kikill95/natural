6d35052929297324f1d53a5f5e0a04628ac90786
Light <kirill.gusyatin@ekreative.com>
2017-06-22 11:53:06 +0300

    Try doubleMetaphone and dice-coefficient

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const natural = require('natural')
+const arr = [
 
-let result = natural.JaroWinklerDistance('Person Surname', 'Preason Surface')
-console.log(result)
+]
+
+const doubleMetaphone = require('double-metaphone')
+const dice = require('dice-coefficient')
+
+arr.forEach(element => {
+  let value = dice(doubleMetaphone(element[0]), doubleMetaphone(element[1])) - 0.6
+  if (value > 0) {
+    console.log(element[0], '|', element[1], ':', value * 2.5)
+  } else {
+    console.log(element[0], '|', element[1], 'Too small')
+  }
+})
